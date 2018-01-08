f80d56ebbdc19a750555aff7eb31a5ee2a167415
srackley <shelbyrackleyart@gmail.com>
2018-01-08 14:53:50 -0600

    create service registry

---
+++ server/serviceRegistry.js
@@ -0,0 +1,52 @@
+'use strict';
+
+class ServiceRegistry {
+  constructor() {
+    this._services = [];
+    this._timeout = 30;
+  }
+
+  add(intent, ip, port) {
+    const key = intent + ip + port;
+    if (!this._services[key]) {
+      this._services[key] = {
+        intent,
+        port,
+        ip,
+        timestamp: Math.floor(new Date() / 1000)
+      };
+
+      console.log(`Added service for intent ${intent} on ${ip}:${port}`);
+      this._cleanup();
+      return;
+    }
+    this._services[key].timestamp = Math.floor(new Date() / 1000);
+    console.log(`Updated service for intent ${intent} on ${ip}:${port}`);
+    this._cleanup();
+  }
+
+  remove(intent, ip, port) {
+    const key = intent + ip + port;
+    delete this._services[key];
+  }
+
+  get(intent) {
+    this._cleanup();
+    for (const key in this._services) {
+      if (this._services[key].intent === intent) return this._services[key];
+    }
+    return null;
+  }
+
+  _cleanup() {
+    const now = Math.floor(new Date() / 1000);
+    for (const key in this._services) {
+      if (this._services[key].timestamp + this._timeout < now) {
+        console.log(`Removed services for intent ${this._services[key].intent}`);
+        delete this._services[key];
+      }
+    }
+  }
+}
+
+module.exports = ServiceRegistry;
