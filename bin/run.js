d2cea1d29b005ea371678614b3b1cd0011cdb9a2
srackley <shelbyrackleyart@gmail.com>
2018-01-07 21:16:09 -0600

    change API token

--- bin/run.js
+++ bin/run.js
@@ -1,12 +1,13 @@
 'use strict';
 
+const slackToken = require('../secrets');
+
 const slackClient = require('../server/slackClient');
 const http = require('http');
 const service = require('../server/service');
 
 const server = http.createServer(service);
 
-const slackToken = 'xoxb-294815327697-a4HU9vSWQohChNzbmy5Ywhj7';
 const slackLogLevel = 'verbose';
 
 const rtm = slackClient.init(slackToken, slackLogLevel);
@@ -15,5 +16,5 @@ rtm.start();
 slackClient.addAuthenticatedHandler(rtm, () => server.listen(3000));
 
 server.on('listening', () => {
-  console.log(`IRIS is listening on ${server.address().port} in ${service.get('env')} mode.`);
+  console.log(`BOT is listening on ${server.address().port} in ${service.get('env')} mode.`);
 });
