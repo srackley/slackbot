d513dff38facd42e1173008a4324bd44ef3788b5
srackley <shelbyrackleyart@gmail.com>
2018-01-07 21:21:54 -0600

    Send and receive test messages

--- server/slackClient.js
+++ server/slackClient.js
@@ -4,12 +4,17 @@ const { RtmClient } = require('@slack/client');
 const { CLIENT_EVENTS } = require('@slack/client');
 const { RTM_EVENTS } = require('@slack/client');
 
+let rtm = null;
+
 function handleOnAuthenticated(rtmStartData) {
   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 }
 
 function handleOnMessage(message) {
   console.log(message);
+  rtm.sendMessage('test', message.channel, () => {
+
+  });
 }
 
 function addAuthenticatedHandler(rtm, handler) {
@@ -17,7 +22,7 @@ function addAuthenticatedHandler(rtm, handler) {
 }
 
 module.exports.init = function slackClient(token, logLevel) {
-  const rtm = new RtmClient(token, { logLevel });
+  rtm = new RtmClient(token, { logLevel });
   addAuthenticatedHandler(rtm, handleOnAuthenticated);
   rtm.on(RTM_EVENTS.MESSAGE, handleOnMessage);
   return rtm;
