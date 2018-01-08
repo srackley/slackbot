eb41b7f2425e689d237b46fa9fcc49e392f34072
srackley <shelbyrackleyart@gmail.com>
2018-01-08 10:00:06 -0600

    made intents dynamic and more modular

--- server/slackClient.js
+++ server/slackClient.js
@@ -1,8 +1,14 @@
 'use strict';
 
-const { RtmClient } = require('@slack/client');
-const { CLIENT_EVENTS } = require('@slack/client');
-const { RTM_EVENTS } = require('@slack/client');
+const {
+  RtmClient
+} = require('@slack/client');
+const {
+  CLIENT_EVENTS
+} = require('@slack/client');
+const {
+  RTM_EVENTS
+} = require('@slack/client');
 
 let rtm = null;
 let nlp = null;
@@ -12,19 +18,33 @@ function handleOnAuthenticated(rtmStartData) {
 }
 
 function handleOnMessage(message) {
-  nlp.ask(message.text, (err, res) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    if (!res.intent) {
-      return rtm.sendMessage('Sorry, I do not know what you are talking about.', message.channel);
-    } else if (res.intent[0].value === 'time' && res.location) {
-      return rtm.sendMessage(`I do not yet know the time in ${res.location[0].value}.`, message.channel);
-    }
-    console.log(res);
-    return rtm.sendMessage('Sorry, I did not understand', message.channel);
-  });
+  if (message.text.toLowerCase().includes('bot')) {
+    nlp.ask(message.text, (err, res) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      try {
+        if (!res.intent || !res.intent[0] || !res.intent[0].value) {
+          throw new Error('Could not extract intent');
+        }
+        const intent = require(`./intents/${res.intent[0].value}Intent`);
+
+        intent.process(res, (error, response) => {
+          if (error) {
+            console.log(error.message);
+            return;
+          }
+          return rtm.sendMessage(response, message.channel);
+        });
+      } catch (err) {
+        console.log(err);
+        console.log(res);
+        rtm.sendMessage("Sorry, I don't know what you are talking about", message.channel);
+      }
+    });
+  }
 }
 
 function addAuthenticatedHandler(rtm, handler) {
@@ -32,7 +52,9 @@ function addAuthenticatedHandler(rtm, handler) {
 }
 
 module.exports.init = function slackClient(token, logLevel, nlpClient) {
-  rtm = new RtmClient(token, { logLevel });
+  rtm = new RtmClient(token, {
+    logLevel
+  });
   nlp = nlpClient;
   addAuthenticatedHandler(rtm, handleOnAuthenticated);
   rtm.on(RTM_EVENTS.MESSAGE, handleOnMessage);
